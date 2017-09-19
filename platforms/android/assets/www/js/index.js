6debb84b459296398e0b60d2a818fde6de1e2471
valentin4311 <valentin.legeay@gmail.com>
2017-09-19 23:55:19 +0200

    V1

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,3 +1,12 @@
+var loginAdress = "https://sts.lecnam.net/idp/Authn/UserPassword";
+var planningAdress = "https://iscople.gescicca.net/Planning.aspx";
+
+//Material colors
+var eventColors = ["#F44336", "#673AB7", "#3F51B5", "#2196F3", "#009688", "#4CAF50", "#8BC34A", "#CDDC39", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722", "#795548"];
+var days = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+var month = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+var dp;
+
 var app =
 {
     // Application Constructor
@@ -8,62 +17,296 @@ var app =
 
     onDeviceReady: function()
     {
-        console.log("Ready");
-
-        $("#loadForm").submit(function(e)
+        var permanentStorage = window.localStorage;
+        var uid = permanentStorage.getItem("uid");
+        if(uid != null)
         {
-            e.preventDefault();
-            $("#loadForm").hide();
-            console.log("Submitted");
-            loadPlanning();
+            this.onLogged();
+        }
+        else
+        {
+            $("#page1").show();
+
+            $("#connectForm").submit(function(e)
+            {
+                e.preventDefault();
+                $("#loginButton")[0].disabled = true;
+
+                //Write data into invisible iframe
+                var iframe = $("iframe").contents();
+
+                //Check is login page
+                if($("#login", iframe) != null)
+                {
+                    $("input[name='j_username']", iframe).val($("#loginUsername").val());
+                    $("input[name='j_password']", iframe).val($("#loginPassword").val());
+                    $("#login", iframe).submit();
+                }
+            });
+
+            //Handle iframe load
+            $("iframe")[0].onload = function(e)
+            {
+                try
+                {
+                    var iframe = $("iframe").contents();
+                    var url = $("iframe")[0].contentWindow.location.href;
+                    var uidElement = $(".JclickApp", iframe);
+                    console.log(url);
+
+                    //Login fail or init
+                    if(url == loginAdress)
+                    {
+                        var error = $("#login > section > .form-error", iframe);
+                        if(error)
+                        {
+                            $("#loginErrorMessage").html(error);
+                            $("#loginErrorMessage").show();
+                            $("#loginButton")[0].disabled = false;
+                        }
+                    }//Connected
+                    else if(uidElement != null)
+                    {
+                        uidElement = $(".JclickApp", $("iframe").contents());
+                        console.log(uidElement);
+                        console.log(uidElement.length);
+
+                        for(var i = 0; i < uidElement.length; i++)
+                        {
+                            var link = String($(uidElement[i]).attr("data-lien"));
+
+                            //Locate right link
+                            if(link.startsWith(planningAdress))
+                            {
+                                var linkParams = link.match(/.*uid=(.*)&code_scolarite=(.*)&cr=(.*)/);
+                                var permanentStorage = window.localStorage;
+                                permanentStorage.setItem("uid", linkParams[1]);
+                                permanentStorage.setItem("cdsc", linkParams[2]);
+                                permanentStorage.setItem("cr", linkParams[3]);
+                                app.onLogged();
+                            }
+                        }
+
+                    }//Unknow page (for some reason)
+                    else
+                    {
+                        $("#loginErrorMessage").html("Erreur inconnue...<br/>Réesayez plus tard ou videz le cache");
+                        $("#loginErrorMessage").show();
+                        $("#loginButton")[0].disabled = false;
+                    }
+                }//Security model unsupported
+                catch (e)
+                {
+                    console.error(e);
+                    $("#loginErrorMessage").html("Erreur de sécurité, navigateur incompatible");
+                    $("#loginErrorMessage").show();
+                    $("#loginButton")[0].disabled = false;
+                }
+            };
+        }
+    },
+
+    onLogged: function()
+    {
+        $("body").css("min-width", "1200px");
+        $("#page1").hide();
+        $("#page2").show();
+        $("#menuContainer").click(function(e)
+        {
+            var permanentStorage = window.localStorage;
+            $("#lastCache").html(lastCache());
+            $("#userCDSC").html(permanentStorage.getItem("cdsc"));
+            $(".overlayBar").css("background-color", "#03A9F4");
+            displayOverlay("#overlayMenu");
         });
 
         $("#previous").click(function(e)
         {
+            $('#previous')[0].disabled = true;
             dp.startDate = dp.startDate.addDays(-7);
             dp.update();
+            updateTopBar();
+            $('#previous')[0].disabled = false;
         });
 
         $("#next").click(function(e)
         {
+            $('#next')[0].disabled = true;
             dp.startDate = dp.startDate.addDays(7);
             dp.update();
+            updateTopBar();
+            $('#next')[0].disabled = false;
         });
-    },
-};
 
-var planningAdress = "https://iscople.gescicca.net/Planning.aspx";
-var dp;
+        $("#okButton").click(function(e)
+        {
+            $("#overlayPannel").hide();
+        });
+
+        $("#retryButton").click(function(e)
+        {
+            $('#retryButton')[0].disabled = true;
+            loadPlanning();
+        });
+
+        $("#reloadButton").click(function(e)
+        {
+            $('#reloadButton')[0].disabled = true;
+            loadPlanning();
+        });
+
+        $("#overlayPannel").click(function(e)
+        {
+            if(e.target == $("#overlayPannel")[0])
+            {
+                $("#overlayPannel").hide();
+            }
+        });
+
+        window.addEventListener("scroll", scrollTopBar);
+        window.addEventListener("resize", scrollTopBar);
+
+        loadPlanning();
+    }
+};
 
 function loadPlanning()
 {
-    $.post(planningAdress, {uid: "9bd54ce86a0f1b1810166554a9ff903a", code_scolarite:"NAQ400940", cr:"NAQ"}, function(data)
-    {
-        dp = new DayPilot.Calendar("planning");
-        dp.viewType = "Week";
-        dp.locale = "fr-fr";
-        dp.headerDateFormat = "dddd d MMMM";
-        dp.columnWidthSpec = "Fixed";
-        dp.columnWidth = 80;
-        dp.headerHeightAutoFit = false;
-        dp.headerHeight = 30;
-        dp.theme = "calendar_green";
-        dp.businessBeginsHour = 8;
-        dp.businessEndsHour = 21;
-        dp.heightSpec = "BusinessHours";
-        dp.initScrollPos = '320';
-        initEvents(data);
+    $("#loader").show();
 
-        dp.init();
-        $("#loadForm").remove();
-        $("#controls").show();
-        $("#planning").show();
-        $("#loader").hide();
+    var permanentStorage = window.localStorage;
+    $.post(planningAdress, {uid: permanentStorage.getItem("uid"), code_scolarite:permanentStorage.getItem("cdsc"), cr:permanentStorage.getItem("cr")}, function(data)
+    {
+        displayPlanning(data, true);
+        $("#overlayPannel").hide();
 
     }).fail(function(error)
     {
-        $("#planning").html("Impossible de charger le planning...");
+        var permanentStorage = window.localStorage;
+        var events = JSON.parse(permanentStorage.getItem("events"));
+
+        if(events != null)
+        {
+            displayPlanning(events, false);
+            $("#errorTitle").html("Pas de connection");
+            $("#errorInfo").html("Impossible de se connecter au service de planning...<br/><br/>" +
+            "<span class='overlayLabel'>Dernière version du planning: </span><br/>" + lastCache());
+
+            displayOverlay("#overlayError");
+        }
+        else
+        {
+            $("#errorTitle").html("Pas de connection");
+            $("#errorInfo").html("Impossible de se connecter au service de planning...");
+            displayOverlay("#overlayError");
+            $("#loader").hide();
+        }
     });
+}
+
+function displayOverlay(overlayId)
+{
+    //Re-Enable buttons
+    $('#reloadButton')[0].disabled = false;
+    $('#retryButton')[0].disabled = false;
+
+    $("#overlayError").hide();
+    $("#overlayEventInfo").hide();
+    $("#overlayMenu").hide();
+    $(overlayId).show();
+    $("#overlayPannel").show();
+}
+
+function updateTopBar()
+{
+    var today = new Date(dp.startDate);
+    today.setDate(today.getDate() - dp.days);
+
+    $(".menuDay").each(function(index)
+    {
+        $(this).html(days[index] + " " + toTwoDigits(today.getDate()) + " " + month[today.getMonth()]);
+        today.setDate(today.getDate() + 1);
+    });
+}
+
+function displayPlanning(data, online)
+{
+    dp = new DayPilot.Calendar("planning");
+    dp.viewType = "Week";
+    dp.locale = "fr-fr";
+    dp.headerDateFormat = "dddd d MMMM";
+    dp.columnWidthSpec = "Fixed";
+    dp.columnWidth = 80;
+    dp.headerHeightAutoFit = false;
+    dp.headerHeight = 30;
+    dp.theme = "calendar_green";
+    dp.businessBeginsHour = 8;
+    dp.businessEndsHour = 21;
+    dp.heightSpec = "BusinessHours";
+    dp.drawBlankCells = false;
+    dp.width = "100%";
+    dp.heightSpec = "Full";
+    //dp.height = $(window).width();
+
+    dp.onEventClicked = function(args)
+    {
+        $("#evTitle").html(args.e.data.evTitle);
+        $("#evRoom").html(args.e.data.evRoom);
+        $("#evTeacher").html(args.e.data.evTeacher);
+        $("#evModality").html(args.e.data.evModality);
+        $("#evType").html(args.e.data.evType);
+        $("#evDate").html(args.e.data.evDate);
+        $("#evUE").html(args.e.data.evUE);
+        $("#evECTS").html(args.e.data.evECTS);
+        $("#evTheme").html(args.e.data.evTheme);
+        $(".overlayBar").css("background-color", args.e.data.backColor);
+        $("#evTitle").css("color", idealTextColor(args.e.data.backColor));
+        displayOverlay("#overlayEventInfo");
+    };
+
+    if(online)
+    {
+        initEvents(data);
+    }
+    else
+    {
+        dp.events.list = data;
+    }
+
+    dp.init();
+    $("#loadForm").remove();
+    $("#controls").show();
+
+    //Fix height
+    var totalHeight = ($($("#planning").children("div")[1]).height() + 30);
+    $("#planning").css("height", totalHeight + "px");
+    $("body").css("height", (totalHeight + 10) + "px");
+    $("#leftBar").css("height", (totalHeight - 30) + "px");
+
+    $("#planning").show();
+    $("#loader").hide();
+
+    var columnWidthStr = $(".calendar_green_colheader").css("width");
+    var columnWidth = parseInt(columnWidthStr.substr(0, columnWidthStr.length - 1));
+    $(".menuDay").each(function(index)
+    {
+        $(this).css("width", columnWidth + "px");
+        $(this).css("left", (45 + columnWidth * index) + "px");
+    });
+    updateTopBar();
+    $("#topBarInternal").show();
+
+    //Display hours
+    $("#leftBar").html("");
+    for(var i = 0; i < 24; i++)
+    {
+        $("#leftBar").append("<div class='leftBarRow'>" + i + "h</div>");
+    }
+    $("#leftBar").show();
+
+    //Re-enable update button
+    $('#reloadButton')[0].disabled = false;
+    $('#retryButton')[0].disabled = false;
 }
 
 function initEvents(data)
@@ -72,46 +315,100 @@ function initEvents(data)
     var dataHtml = $(data);
     $(".infobulle", dataHtml).each(function(eventId)
     {
+        //Full title
         var title = this.innerHTML.split("<br>", 1)[0];
+
+        //Title array [UE, TITLE, ECTS]
         var titleStruct = title.split(": ", 2);
+        titleStruct[2] = "";
 
-        var nEvent = {id: eventId, start: "", end: "", text: titleStruct[1]};
-
-        if(title.length >= 6)
+        // ECTS
+        var ectsMatch = titleStruct[1].match(/\([0-9]* ECTS\)/);
+        if(ectsMatch != null)
         {
-            var red = (title.charCodeAt(0) + title.charCodeAt(1)) % 128 + 64;
-            var green = (title.charCodeAt(2) + title.charCodeAt(3)) % 128 + 64;
-            var blue = (title.charCodeAt(4) + title.charCodeAt(5)) % 128 + 64;
-
-            nEvent.backColor = rgbToHex(red, green, blue);
+            titleStruct[2] = ectsMatch[0];
+            titleStruct[1] = titleStruct[1].substr(0, titleStruct[1].length - ectsMatch[0].length);
         }
 
+
+        //Event color
+        var color = eventColors[Math.abs(title.hashCode() % eventColors.length)];
+
+        var nEvent = {id: eventId, text: "", start: "", end: "", evTitle: titleStruct[1], evRoom: "", evTeacher: "Aucun", evType: "", evModality: "", evDate: "", evTheme: "Aucun de thème indiqué", evUE: titleStruct[0], evECTS: titleStruct[2], fontColor: idealTextColor(color), backColor: color};
+
+        var lastKey = "";
         $("div", this).each(function(divId)
         {
             var text = this.innerHTML;
 
-            // Check is date
-            if(text.match(/[0-9]*\/[0-9]*\/[0-9]* [0-9]*:[0-9]* - [0-9]*:[0-9]*/))
+            // Register values from HTML code
+            if(lastKey == " Modalite :  ")
             {
+                nEvent.evModality = text;
+            }
+            else if(lastKey == " Type :  ")
+            {
+                nEvent.evType = text;
+            }
+            else if(lastKey == " Tuteur :  ")
+            {
+               nEvent.evTeacher = text;
+            }
+            else if(lastKey == " Salle :  ")
+            {
+               nEvent.evRoom = text;
+            }
+            else if(lastKey == " Thématique : ")
+            {
+               nEvent.evTheme = text;
+            }
+            else if(lastKey == " Date et horaire :  ")
+            {
+                nEvent.evDate = text;
+
                 var dateData = text.split(" ");
                 var dayData = dateData[0].split("/");
                 nEvent.start = dayData[2] + "-" + dayData[1] + "-" + dayData[0] + "T" + dateData[1] + ":00"
                 nEvent.end = dayData[2] + "-" + dayData[1] + "-" + dayData[0] + "T" + dateData[3] + ":00"
             }
+
+            lastKey = text;
         });
+        nEvent.text = nEvent.evTitle + "<br/>" +
+        "<img class='eventIcon' src='./img/room.png'/><span class='eventLabel'>" + nEvent.evRoom + "</span><br/>" +
+        "<img class='eventIcon' src='./img/teacher.png'/><span class='eventLabel'>" + nEvent.evTeacher + "</span><br/>" +
+        "<img class='eventIcon' src='./img/ue.png'/><span class='eventLabel'>" + nEvent.evUE + "</span><br/>" +
+        "<img class='eventIcon' src='./img/type.png'/><span class='eventLabel'>" + nEvent.evType + "</span>";
         dp.events.list.push(nEvent);
     });
+
+    var permanentStorage = window.localStorage;
+    permanentStorage.setItem("events", JSON.stringify(dp.events.list));
+    permanentStorage.setItem("lastUpdate", new Date().getTime());
 }
 
-function componentToHex(c)
+function scrollTopBar(e)
 {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
+    var supportPageOffset = window.pageXOffset !== undefined;
+    var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+
+    window.addEventListener("scroll", function(e) {
+
+    /* A full compatability script from MDN for gathering the x and y values of scroll: */
+    var x = supportPageOffset ? window.pageXOffset : isCSS1Compat ? document.documentElement.scrollLeft : document.body.scrollLeft;
+    var y = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+
+    $("#topBar")[0].style.left = -x + "px";
+    $("#topBar")[0].style.width = $(window).width() + x + "px";
+  });
 }
 
-function rgbToHex(r, g, b)
+function lastCache()
 {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    var permanentStorage = window.localStorage;
+    var lastUpdate = new Date(parseInt(permanentStorage.getItem("lastUpdate")));
+    return toTwoDigits(lastUpdate.getDate()) + "/" + toTwoDigits(lastUpdate.getMonth() + 1) + "/" + lastUpdate.getFullYear() +
+    " " + toTwoDigits(lastUpdate.getHours()) + ":" + toTwoDigits(lastUpdate.getMinutes());
 }
 
 $(function()
---
+++ platforms/android/assets/www/js/utils.js
@@ -0,0 +1,59 @@
+function componentToHex(c)
+{
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b)
+{
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function toTwoDigits(value)
+{
+    if(value < 10)
+    {
+        return "0" + value;
+    }
+    return value;
+}
+
+String.prototype.hashCode = function()
+{
+    var hash = 0, i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+        chr   = this.charCodeAt(i);
+        hash  = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+};
+
+String.prototype.startsWith = function(needle)
+{
+    return(this.indexOf(needle) == 0);
+};
+
+function idealTextColor(bgColor)
+{
+
+   var nThreshold = 105;
+   var components = getRGBComponents(bgColor);
+   var bgDelta = (components.R * 0.299) + (components.G * 0.587) + (components.B * 0.114);
+
+   return ((255 - bgDelta) < nThreshold) ? "#000000" : "#ffffff";
+}
+
+function getRGBComponents(color)
+{
+    var r = color.substring(1, 3);
+    var g = color.substring(3, 5);
+    var b = color.substring(5, 7);
+
+    return {
+       R: parseInt(r, 16),
+       G: parseInt(g, 16),
+       B: parseInt(b, 16)
+    };
+}
